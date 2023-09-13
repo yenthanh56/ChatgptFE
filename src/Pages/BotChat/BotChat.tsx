9595a6d1440641732e66fbfe9c6eba14cbb5e5fb
yenthanh56 <57222518+yenthanh56@users.noreply.github.com>
2023-09-13 16:34:45 +0700

    update

--- src/Pages/BotChat/BotChat.tsx
+++ src/Pages/BotChat/BotChat.tsx
@@ -48,33 +48,31 @@ export const BotChat = () => {
 	const sendPrompt = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
-		if (messages) {
-			setMessages((prevMessages) => [
-				...prevMessages,
-				{ id: generateUID(), name: "me", text: prompt },
-			]);
-			setPrompt("");
-			setisLoading(true);
+		setMessages((prevMessages) => [
+			...prevMessages,
+			{ id: generateUID(), name: "me", text: prompt },
+		]);
+		setPrompt("");
+		setisLoading(true);
 
-			await fetch("https://chatgpt-be.vercel.app/v1/bot", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ prompt: prompt }),
-			})
-				.then((res) => res.json())
-				.then((data) =>
-					setMessages((prevMessages) => [
-						...prevMessages,
-						{
-							id: generateUID(),
-							name: "Bot",
-							text: data.bot,
-						},
-					])
-				);
-		}
+		await fetch("https://chatgpt-be.vercel.app/v1/bot", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ prompt: prompt }),
+		})
+			.then((res) => res.json())
+			.then((data) =>
+				setMessages((prevMessages) => [
+					...prevMessages,
+					{
+						id: generateUID(),
+						name: "Bot",
+						text: data.bot,
+					},
+				])
+			);
 
 		setisLoading(false);
 	};
@@ -138,7 +136,7 @@ export const BotChat = () => {
 					/>
 					<button
 						style={{ border: "none" }}
-						disabled={messages ? false : true}
+						disabled={prompt && messages ? false : true}
 					>
 						<IconButton
 							type="button"
